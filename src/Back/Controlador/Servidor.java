a0bc2653d81a90e43051d0139d7307225ff3de5c
Santiago Velandia Gallo <santiago.velandiag@uqvirtual.edu.co>
2023-03-16 00:37:45 -0500

    Realizo ajustes en las validaciones

---
+++ src/Back/Controlador/Servidor.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Back.Controlador;
+
+import Back.Modelo.Banco;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+public class Servidor {
+
+    public static void main(String[] args) {
+
+        ServerSocket listener = null;
+        Socket serverSideSocket = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        //puerto de nuestro servidor
+        final int PUERTO = 5000;
+
+        try {
+            //Creamos el socket del servidor
+            listener = new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado");
+            Banco miBanco = new Banco();
+            //Siempre estara escuchando peticiones
+            while (true) {
+
+                //Espero a que un cliente se conecte
+                serverSideSocket = listener.accept();
+
+                System.out.println("Cliente conectado");
+                in = new DataInputStream(serverSideSocket.getInputStream());
+                out = new DataOutputStream(serverSideSocket.getOutputStream());
+
+                //Leo el mensaje que me envia
+                String mensaje = in.readUTF();
+                System.out.println(mensaje);
+                String[] solicitud = mensaje.split("&");
+                double resultado;
+
+                switch (solicitud[0]) {
+                    case "0":
+                        try {
+                            out.writeUTF(miBanco.ingresar(solicitud[1], Integer.parseInt(solicitud[2])) ? "200" : "400");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "1":
+                        try {
+                            System.out.println("opcion 1: registrar cuenta");
+                            out.writeUTF("Número de cuenta asignado: #" + miBanco.abrirCuenta(solicitud[1], solicitud[2], solicitud[3], Integer.parseInt(solicitud[4]), Double.parseDouble(solicitud[5])));
+
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "2":
+                        try {
+
+                            System.out.println("opcion 2: modificar cuenta");
+                            out.writeUTF(miBanco.modificarCuenta(Integer.parseInt(solicitud[1]), solicitud[2], solicitud[3], solicitud[4]) ? "Modificación exitosa" : "Error desconocido en la operación");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "3":
+                        try {
+
+                            out.writeUTF(miBanco.cerrarCuenta(Integer.parseInt(solicitud[1]), solicitud[2], Integer.parseInt(solicitud[3])) ? "Cancelación de cuenta exitosa" : "Error desconoscido en la operación");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "4":
+                        try {
+
+                            resultado = miBanco.consignarDinero(Integer.parseInt(solicitud[1]), solicitud[2], Double.parseDouble(solicitud[3]));
+                            out.writeUTF((resultado > -1) ? "Consignación exitosa \n su saldo actual es: " + resultado : "Error en la operación");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "5":
+                        try {
+
+                            resultado = miBanco.transferirDinero(Integer.parseInt(solicitud[1]), Double.parseDouble(solicitud[2]), Integer.parseInt(solicitud[3]), Integer.parseInt(solicitud[4]));
+                            out.writeUTF((resultado > -1) ? "Tranferencia exitosa \n ahora el remitente tiene: " + resultado : "Error en la operación");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "6":
+                        try {
+
+                            resultado = miBanco.retirarDinero(Integer.parseInt(solicitud[1]), solicitud[2], Double.parseDouble(solicitud[3]), Integer.parseInt(solicitud[4]));
+                            out.writeUTF((resultado > -1) ? "Transacción exitosa \n su saldo actual es: " + resultado : "Error en la operación");
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+
+                    case "7":
+                        try {
+
+                            out.writeUTF("" + miBanco.getUsuariosToString());
+                        } catch (Exception e) {
+                            out.writeUTF("Error --> " + e);
+                        }
+                        break;
+                }
+                try {
+                    if (Integer.parseInt(solicitud[0]) > 6) {
+                        out.writeUTF("¡Error! no se ha interpretado la solicitud"
+                                + "\n debido a que no es una opción válida");
+                    }
+
+                } catch (NullPointerException e) {
+                    out.writeUTF("¡Error! no se ha interpretado la solicitud \n"
+                            + "Debido a que no ha ingresado un número: \n" + e);
+                }
+                //Cierro el socket
+                serverSideSocket.close();
+                System.out.println("Cliente desconectado");
+                miBanco.escribirEnArchivo();
+                System.out.println("Información guardada");
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
---
+++ src/Back/Modelo/Banco.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Back.Modelo;
+
+import Back.Modelo.Cuenta;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author USER
+ */
+public class Banco {
+
+    public static ArrayList<Cuenta> cuentasUsuarios;
+
+    public Banco() throws IOException {
+        cuentasUsuarios = new ArrayList<>(); //NOMBRE, APELLIDO, CEDULA, CLAVE, MONTO EN LA CUENTA, NUMERO DE CUENTA
+        cuentasUsuarios.add(new Cuenta("Santiago", "Velanida Gallo", "1094971551", 1234, 8000, 1));
+        cuentasUsuarios.add(new Cuenta("Estefania", "Arroyave Bermudez", "1094953679", 4321, 400000, 2));
+        escribirEnArchivo();
+
+    }
+    
+    public static void escribirEnArchivo() throws IOException {
+        // Crea una instancia de FileWriter para escribir en el archivo de texto
+        FileWriter fileWriter = new FileWriter("src/Recursos_Datos/banco_Cuentas.txt");
+
+        // Crea una instancia de BufferedWriter para escribir en el archivo de texto
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        // Itera sobre el arreglo de objetos y escribe cada objeto en una línea separada en el archivo de texto
+        bufferedWriter.write("numCuenta,nombre,apellido,cedula,deposito");
+        bufferedWriter.newLine();
+        for (Cuenta usuario : cuentasUsuarios) {
+            bufferedWriter.write(usuario.toString()+";");
+            bufferedWriter.newLine();
+        }
+
+        // Cierra el BufferedWriter para asegurarte de que todos los datos se han escrito en el archivo de texto
+        bufferedWriter.close();
+    }
+
+    public String getUsuariosToString(){
+        String usuarios = "";
+        for (Cuenta usuario : cuentasUsuarios) {
+            usuarios = usuarios+usuario.getNombre()+"&"+usuario.getApellido()
+                    +"&"+usuario.getCedula()+"&"+usuario.getDeposito()+"&"+
+                    usuario.getNumeroCuenta()+"%";
+        }
+        return usuarios;
+    }
+    
+    public boolean ingresar(String cedula, int clave) {
+        for (Cuenta usuario : cuentasUsuarios) {
+            if (usuario.getCedula().equals(cedula) && usuario.getClave() == clave) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int abrirCuenta(String nombre, String apellido, String cedula, int clave, double deposito) {
+        try {
+            
+            Cuenta nuevoUsuario = new Cuenta(nombre, apellido, cedula, clave, deposito, cuentasUsuarios.size() + 101);
+            cuentasUsuarios.add(nuevoUsuario);
+            return nuevoUsuario.getNumeroCuenta();
+        } catch (Exception e) {
+            System.out.println("Erro al registrar cuenta: "+e);
+            return -1;
+        }
+    }
+
+    public boolean modificarCuenta(int clave, String cedula, String cambio, String nuevoValor) {
+        int posCuenta = getPosicionPorCedula(cedula);
+        if (posCuenta >= 0) {
+            switch (cambio) {
+                case "Nombre":
+                    cuentasUsuarios.get(posCuenta).setNombre(nuevoValor);
+                    return true;
+                case "Apellido":
+                    cuentasUsuarios.get(posCuenta).setApellido(nuevoValor);
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean cerrarCuenta(int cuenta, String motivo, int clave) {
+        int posCuenta = getPosicionPorCuenta(cuenta);
+        if (posCuenta >= 0) {
+            if (cuentasUsuarios.get(posCuenta).getClave() == clave) {
+                cuentasUsuarios.remove(posCuenta);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public double consignarDinero(int cuenta, String cedula, double deposito) {
+        int posCuenta = getPosicionPorCuenta(cuenta);
+        if (posCuenta >= 0) {
+            Cuenta user = cuentasUsuarios.get(posCuenta);
+            if (cedula.equals(user.getCedula())) {
+                double dineroActual = user.getDeposito();
+                user.setDeposito(dineroActual + deposito);
+                return user.getDeposito();
+            }
+        }
+        return -1;
+    }
+
+    public double transferirDinero(int cuentaDestino, double transferencia, int cuentaOrigen, int clave) {
+        int posCuentaDestino = getPosicionPorCuenta(cuentaDestino);
+        int posCuentaOrigen = getPosicionPorCuenta(cuentaOrigen);
+        if (posCuentaDestino >= 0 && posCuentaOrigen >= 0 && posCuentaDestino != posCuentaOrigen) {
+            Cuenta userD = cuentasUsuarios.get(posCuentaDestino);
+            Cuenta userO = cuentasUsuarios.get(posCuentaOrigen);
+            if (transferencia <= userO.getDeposito() && clave == userO.getClave()) {
+                double dineroActualUserO = userO.getDeposito();
+                double dineroActualUserD = userD.getDeposito();
+                userO.setDeposito(dineroActualUserO - transferencia);
+                userD.setDeposito(dineroActualUserD + transferencia);
+
+                return dineroActualUserO - transferencia;
+            }
+        }
+        return -1;
+    }
+
+    public double retirarDinero(int cuenta, String cedula, double retiro, int clave) {
+        int posCuenta = getPosicionPorCuenta(cuenta);
+        if (posCuenta >= 0) {
+            Cuenta user = cuentasUsuarios.get(posCuenta);
+            if (cedula.equals(user.getCedula()) && clave == user.getClave()) {
+                double dineroActual = user.getDeposito();
+                if (dineroActual >= retiro) {
+                    user.setDeposito(dineroActual - retiro);
+                    return user.getDeposito();
+                }
+            }
+        }
+        return -1;
+    }
+
+    private int getPosicionPorCuenta(int cuenta) {
+        for (int i = 0; i < cuentasUsuarios.size(); i++) {
+            if (cuenta == cuentasUsuarios.get(i).getNumeroCuenta()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getPosicionPorCedula(String cedula) {
+        for (int i = 0; i < cuentasUsuarios.size(); i++) {
+            if (cedula.equals(cuentasUsuarios.get(i).getCedula())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
